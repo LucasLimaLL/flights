956528c4ee380877669e8c3e64d36233a8eaa66d
Lucas Lima <lucaslima_miml@hotmail.com>
2025-01-01 20:51:11 -0300

    chore(start): include amadeus call

--- src/main/java/br/com/lucaslima/flights/adapters/web/out/amadeus/auth/properties/AuthProperties.java
+++ src/main/java/br/com/lucaslima/flights/adapters/web/out/amadeus/auth/properties/AuthProperties.java
@@ -1,10 +1,12 @@
 package br.com.lucaslima.flights.adapters.web.out.amadeus.auth.properties;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
+@Setter
 @Configuration
 @ConfigurationProperties(prefix = "http.amadeus.auth")
 public class AuthProperties {
