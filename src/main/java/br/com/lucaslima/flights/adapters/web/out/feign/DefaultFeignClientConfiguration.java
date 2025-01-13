af3c129c6850ee1290da18fbdbafcb802f48a0e6
Lucas Lima <lucaslima_miml@hotmail.com>
2025-01-13 20:07:56 -0300

    chore(feign): include retry for 429

--- src/main/java/br/com/lucaslima/flights/adapters/web/out/feign/DefaultFeignClientConfiguration.java
+++ src/main/java/br/com/lucaslima/flights/adapters/web/out/feign/DefaultFeignClientConfiguration.java
@@ -1,14 +1,24 @@
 package br.com.lucaslima.flights.adapters.web.out.feign;
 
+import feign.Retryer;
 import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+
 @Configuration
 public class DefaultFeignClientConfiguration {
+
+    private static final int MAX_ATTEMPTS = 5;
 
     @Bean
     public OkHttpClient okHttpClient() {
         return new OkHttpClient();
+    }
+
+    @Bean
+    public Retryer retryer() {
+        return new Retryer.Default(100, SECONDS.toMillis(1), MAX_ATTEMPTS);
     }
 }
